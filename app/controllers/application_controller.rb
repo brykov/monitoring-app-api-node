5c24633b7b5b28101a9ea4e787318dfc3afa643b
Ivan Brykov <ivan.ru@gmail.com>
2016-01-31 17:18:51 +0300

    shutdown function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,31 @@
 class ApplicationController < ActionController::API
+
+  rescue_from
+
   def stats
     usw = Usagewatch
     render json: {
                uw_diskused: usw.uw_diskused,
                uw_diskused_perc: usw.uw_diskused_perc,
                uw_cpuused: usw.uw_cpuused,
-               # uw_tcpused: usw.uw_tcpused,
-               # uw_udpused: usw.uw_udpused,
                uw_memused: usw.uw_memused,
                uw_load: usw.uw_load,
-               # uw_bandrx: usw.uw_bandrx,
-               # uw_bandtx: usw.uw_bandtx,
-               # uw_diskioreads: usw.uw_diskioreads,
-               # uw_diskiowrites: usw.uw_diskiowrites,
                uw_cputop: usw.uw_cputop,
                uw_memtop: usw.uw_memtop,
            }
   end
 
   def shutdown
-    system('sudo poweroff')
+    authenticate!
+    system('sudo poweroff &')
+    render text: 'OK'
   end
+
+  private
+
+  def authenticate!
+    raise 'Unauthorized' unless params.require('auth_token')=='df351d97acbd7d3f7e8cabfa884315ebb5dc336f4062496c40dacba8222907f72d3e199f79c989fef040ff9b18a3b456d2d4237b3f30ddd07404f7a28c8b29a9'
+  end
+
+
 end
